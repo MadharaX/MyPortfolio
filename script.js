9667bc8a4fd00d4943eda725ed400d4173a79af6
MadharaX <madharamithila@gmail.com>
2025-01-21 15:10:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMenu() {
+    var menu = document.getElementById('headerS1P1');
+    menu.classList.toggle('open');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var backToTopLink = document.getElementById('backToTopLink');
+    if (backToTopLink) {
+        backToTopLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            smoothScrollToTop();
+        });
+    }
+});
+
+function smoothScrollToTop() {
+    const scrollDuration = 200; // Duration in milliseconds
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+    const scrollInterval = setInterval(function() {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, 15);
+}
